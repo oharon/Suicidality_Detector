fb699615dfd0703e9bf83ff1bf815c88ab51cfee
oharon <oharon@naver.com>
2023-06-13 16:54:03 +0200

    Update app.py

--- api/app.py
+++ api/app.py
@@ -12,7 +12,7 @@ h1 {
     color: white;
 }
 .stApp {
-    background-image: url(https://unsplash.com/photos/21jDy_yt008);
+    background-image: url(https://images.unsplash.com/photo-1517921076612-169b401c825e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3870&q=80);
     background-size: cover;
 }
 """
