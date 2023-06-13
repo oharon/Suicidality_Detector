a1237d386e861d5d946e188039012b9a3f3e370c
oharon <oharon@naver.com>
2023-06-13 11:40:49 +0200

    Update fast.py

--- api/fast.py
+++ api/fast.py
@@ -1,25 +1,23 @@
-import pandas as pd
-import numpy as np
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from fastapi.middleware.cors import CORSMiddleware
-# from transformers import DistilBertTokenizer, TFDistilBertForSequenceClassification
-from transformers import TFRobertaModel, RobertaTokenizer
-import tensorflow as tf
-import tensorflow_addons as tfa
-from ml_logic.registry import load_model
+from ml_logic.data import preprocessing
+from params import *
+from pydantic import BaseModel
+from transformers import TFDistilBertForSequenceClassification, TFRobertaForSequenceClassification
+import json
+from pathlib import Path
+import os
 
-# from Suicidality_Detector.ml_logic.model import load_model
+class Item(BaseModel):
+    category: str
+    proba: float
 
 app = FastAPI()
 
 @app.get('/')
 def index():
     return {'message': 'Hello, World'}
-
-@app.get('/Welcome')
-def get_name(name: str):
-    return {'Welcome To Suicidality Detector': f'{name}'}
 
 # Allowing all middleware is optional, but good practice for dev purposes
 app.add_middleware(
@@ -30,51 +28,32 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
-@app.get("/preprocess")
-def preprocess(user_text):
-    inputs = str(user_text)
+# http://127.0.0.1:8000/predict?post=string
+@app.get('/predict')
+def predict(post):
+    print('Prediction API Call started...')
 
-    tokenizer, model = load_model()
-    print('model is loaded')
-    assert tokenizer is not None
+    # this pred() function needs to be fixed
 
-    # Tokenize the input sentence
-    inputs = tokenizer(inputs, truncation=True, padding=True, return_tensors="tf")
-    return inputs
+    loaded_model = TFRobertaForSequenceClassification.from_pretrained('roberta-base', num_labels=5)
+    model_dir= os.path.join(Path(os.getcwd()).parent.absolute(), 'models')
+    loaded_model.load_weights(os.path.join(model_dir, 'weights', 'weights'))
 
+    encoded = preprocessing([post])
+    input_ids = encoded['input_ids']
+    attention_mask = encoded['attention_mask']
+    predicted = loaded_model.predict([input_ids, attention_mask])
 
-@app.get("/predict")
-def predict(
-        user_text :str,  # ex) I wanna kill myself :(
-    ):
+    probabilities = predicted['logits']
 
-    tokenizer, model = load_model()
+    max_val = probabilities.argmax()
+    max_val_p = probabilities.max()
 
-    inputs = preprocess(user_text)
+    print(f'highest possibility is {max_val_p} on category {max_val}')
+    returned = [
+        {'max_val' : max_val, 'max_val_p' : max_val_p},
+        ]
+    json_str = json.dumps(returned, indent=4, default=str)
+    return Response(content=json_str, media_type='application/json')
 
-    # Make the prediction
-    logits = model(inputs.input_ids, attention_mask=inputs.attention_mask).logits
-    probabilities = tf.nn.softmax(logits, axis=1)[0].numpy().tolist()
-
-    # inputs_ids = inputs["input_ids"]
-    # print(inputs_ids)
-    # inputs_attention_mask = inputs["attention_mask"]
-    # print(inputs_attention_mask)
-    # predictions = model.predict([inputs_ids, inputs_attention_mask])
-    # print(predictions)
-
-    # Define class labels
-    labels = [0, 1, 2, 3, 4]
-
-    # Prepare the API response
-    response = {label: prob for label, prob in zip(labels, probabilities)}
-
-    return response
-
-    ## 'response' contains dictionary of scores for: 'Supportive(4)', 'Ideation(2)', 'Behavior(1)', 'Attempt(0)', 'Indicator(3)'
-
-
-
-if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
-#uvicorn main:app --reload
+    ## 'response' contains dictionary of scores for: 'Attempt(0)', 'Behavior(1)', 'Ideation(2)', 'Indicator(3)', 'Supportive(4)'
